99ebb9eb6f9dedfc108ca6e8c7907a7cc8c31018
TootEgozy <tootegozy@gmail.com>
2024-09-25 15:45:07 +0300

    solution

--- 6-Zigzag-Conversion/index.ts
+++ 6-Zigzag-Conversion/index.ts
@@ -4,22 +4,25 @@
  * @return {string}
  */
 const convert = (s: string, numRows: number) => {
-  const collection = Array.from(Array(numRows)).fill([]);
+  const collection = Array.apply(null, Array(numRows)).map((): string[] => []);
 
   let des = true; // is the direction descending?
   let arrLoc = 0; // index of the subarray to push into
 
   s.split('').forEach((char) => {
     collection[arrLoc].push(char);
-    console.log(des);
     if(des) {
       if(collection[arrLoc + 1]) arrLoc += 1;
-      des = false;
-      arrLoc -= 1;
+      else {
+        des = false;
+        arrLoc -= 1;
+      }
     } else {
       if(collection[arrLoc - 1]) arrLoc -= 1;
-      des = true;
-      arrLoc += 1;
+      else {
+        des = true;
+        arrLoc += 1;
+      }
     }
   });
 
@@ -27,6 +30,6 @@ const convert = (s: string, numRows: number) => {
 
 };
 
-convert("abcdefg", 2);
+convert("abcdefghijklmnop", 3);
 
 export { convert }
